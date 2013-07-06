31627175c82c8f6828531098927e92b1443d3294
Tom Kelly <tomkelly000@gmail.com>
2013-07-06 00:45:43 -0400

    fixed clearing

--- routes/clear.js
+++ routes/clear.js
@@ -13,31 +13,31 @@ function clear(app) {
 	    var db = mongodb.connect(mongoUri, ['daily']);
 
 	    if (req.query.name) {
-		db.highscores.remove( {'name':req.query.name},
+		db.daily.remove( {'name':req.query.name},
 				      function() { res.end() });
 	    }
 	    if (req.query.score) {
-		db.highscores.remove( {'score':parseInt(req.query.score)},
+		db.daily.remove( {'score':parseInt(req.query.score)},
 				      function() { res.end() });
 	    }
 	    // remove everything
 	    if (!(req.query.name || req.query.score)) {
-		db.highscores.remove(function() { res.end() });
+		db.daily.remove(function() { res.end() });
 	    }
 
 	    var db = mongodb.connect(mongoUri, ['alltime']);
 
 	    if (req.query.name) {
-		db.highscores.remove( {'name':req.query.name},
+		db.alltime.remove( {'name':req.query.name},
 				      function() { res.end() });
 	    }
 	    if (req.query.score) {
-		db.highscores.remove( {'score':parseInt(req.query.score)},
+		db.alltime.remove( {'score':parseInt(req.query.score)},
 				      function() { res.end() });
 	    }
 	    // remove everything
 	    if (!(req.query.name || req.query.score)) {
-		db.highscores.remove(function() { res.end() });
+		db.alltime.remove(function() { res.end() });
 	    }
 	});	
 }    
--- routes/clear.js~
+++ routes/clear.js~
@@ -10,7 +10,22 @@ var mongoUri = process.env.MONGOLAB_URI ||
 
 function clear(app) {
     app.get('/clear', function(req, res) {
-	    var db = mongodb.connect(mongoUri, ['highscores']);
+	    var db = mongodb.connect(mongoUri, ['daily']);
+
+	    if (req.query.name) {
+		db.highscores.remove( {'name':req.query.name},
+				      function() { res.end() });
+	    }
+	    if (req.query.score) {
+		db.highscores.remove( {'score':parseInt(req.query.score)},
+				      function() { res.end() });
+	    }
+	    // remove everything
+	    if (!(req.query.name || req.query.score)) {
+		db.highscores.remove(function() { res.end() });
+	    }
+
+	    var db = mongodb.connect(mongoUri, ['alltime']);
 
 	    if (req.query.name) {
 		db.highscores.remove( {'name':req.query.name},
